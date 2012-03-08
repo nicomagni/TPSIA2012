73d87c9dda05ac3a2936b2450c9c1e8a8238dd55
Paul Ballesty <paulballesty@gmail.com>
2012-03-08 20:38:08 -0300

    Starter

---
+++ SiaTp1/src/domain/Solver.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class Solver {
+
+	public static void main(String[] args) {
+		new Solver().start();
+	}
+	
+	private void start() {
+		//TODO: Debería hacer algo si queremos aprobar.
+	}
+}
