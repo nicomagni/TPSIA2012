fbbfc14ed6ebf963b51621b6ab4b2472edbbb733
Paul Ballesty <paulballesty@gmail.com>
2012-03-08 20:54:40 -0300

    .project

---
+++ SiaTp1/src/domain/Board.java
@@ -0,0 +1,16 @@
+package domain;
+
+public class Board {
+
+	private int[][] board;
+	int colors;
+	
+	public Board(int rows, int cols, int colors) {
+		this.board = new int[rows][cols];
+	}
+	
+	public void randomFill() {
+		
+	}
+	
+}
